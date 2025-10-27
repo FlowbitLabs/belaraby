8c68e3c65e29cea8902f25bb031053c8140085bb
Hongru Ma <mahongru.dev@gmail.com>
2025-10-27 15:11:52 +0300

    add remote schema migration

---
+++ supabase/migrations/20251027110740_remote_schema.sql
@@ -0,0 +1,380 @@
+revoke delete on table "public"."lesson_exercise_options" from "anon";
+
+revoke insert on table "public"."lesson_exercise_options" from "anon";
+
+revoke references on table "public"."lesson_exercise_options" from "anon";
+
+revoke select on table "public"."lesson_exercise_options" from "anon";
+
+revoke trigger on table "public"."lesson_exercise_options" from "anon";
+
+revoke truncate on table "public"."lesson_exercise_options" from "anon";
+
+revoke update on table "public"."lesson_exercise_options" from "anon";
+
+revoke delete on table "public"."lesson_exercise_options" from "authenticated";
+
+revoke insert on table "public"."lesson_exercise_options" from "authenticated";
+
+revoke references on table "public"."lesson_exercise_options" from "authenticated";
+
+revoke select on table "public"."lesson_exercise_options" from "authenticated";
+
+revoke trigger on table "public"."lesson_exercise_options" from "authenticated";
+
+revoke truncate on table "public"."lesson_exercise_options" from "authenticated";
+
+revoke update on table "public"."lesson_exercise_options" from "authenticated";
+
+revoke delete on table "public"."lesson_exercise_options" from "service_role";
+
+revoke insert on table "public"."lesson_exercise_options" from "service_role";
+
+revoke references on table "public"."lesson_exercise_options" from "service_role";
+
+revoke select on table "public"."lesson_exercise_options" from "service_role";
+
+revoke trigger on table "public"."lesson_exercise_options" from "service_role";
+
+revoke truncate on table "public"."lesson_exercise_options" from "service_role";
+
+revoke update on table "public"."lesson_exercise_options" from "service_role";
+
+revoke delete on table "public"."lesson_exercises" from "anon";
+
+revoke insert on table "public"."lesson_exercises" from "anon";
+
+revoke references on table "public"."lesson_exercises" from "anon";
+
+revoke select on table "public"."lesson_exercises" from "anon";
+
+revoke trigger on table "public"."lesson_exercises" from "anon";
+
+revoke truncate on table "public"."lesson_exercises" from "anon";
+
+revoke update on table "public"."lesson_exercises" from "anon";
+
+revoke delete on table "public"."lesson_exercises" from "authenticated";
+
+revoke insert on table "public"."lesson_exercises" from "authenticated";
+
+revoke references on table "public"."lesson_exercises" from "authenticated";
+
+revoke select on table "public"."lesson_exercises" from "authenticated";
+
+revoke trigger on table "public"."lesson_exercises" from "authenticated";
+
+revoke truncate on table "public"."lesson_exercises" from "authenticated";
+
+revoke update on table "public"."lesson_exercises" from "authenticated";
+
+revoke delete on table "public"."lesson_exercises" from "service_role";
+
+revoke insert on table "public"."lesson_exercises" from "service_role";
+
+revoke references on table "public"."lesson_exercises" from "service_role";
+
+revoke select on table "public"."lesson_exercises" from "service_role";
+
+revoke trigger on table "public"."lesson_exercises" from "service_role";
+
+revoke truncate on table "public"."lesson_exercises" from "service_role";
+
+revoke update on table "public"."lesson_exercises" from "service_role";
+
+revoke delete on table "public"."lesson_grammar" from "anon";
+
+revoke insert on table "public"."lesson_grammar" from "anon";
+
+revoke references on table "public"."lesson_grammar" from "anon";
+
+revoke select on table "public"."lesson_grammar" from "anon";
+
+revoke trigger on table "public"."lesson_grammar" from "anon";
+
+revoke truncate on table "public"."lesson_grammar" from "anon";
+
+revoke update on table "public"."lesson_grammar" from "anon";
+
+revoke delete on table "public"."lesson_grammar" from "authenticated";
+
+revoke insert on table "public"."lesson_grammar" from "authenticated";
+
+revoke references on table "public"."lesson_grammar" from "authenticated";
+
+revoke select on table "public"."lesson_grammar" from "authenticated";
+
+revoke trigger on table "public"."lesson_grammar" from "authenticated";
+
+revoke truncate on table "public"."lesson_grammar" from "authenticated";
+
+revoke update on table "public"."lesson_grammar" from "authenticated";
+
+revoke delete on table "public"."lesson_grammar" from "service_role";
+
+revoke insert on table "public"."lesson_grammar" from "service_role";
+
+revoke references on table "public"."lesson_grammar" from "service_role";
+
+revoke select on table "public"."lesson_grammar" from "service_role";
+
+revoke trigger on table "public"."lesson_grammar" from "service_role";
+
+revoke truncate on table "public"."lesson_grammar" from "service_role";
+
+revoke update on table "public"."lesson_grammar" from "service_role";
+
+revoke delete on table "public"."lesson_keywords" from "anon";
+
+revoke insert on table "public"."lesson_keywords" from "anon";
+
+revoke references on table "public"."lesson_keywords" from "anon";
+
+revoke select on table "public"."lesson_keywords" from "anon";
+
+revoke trigger on table "public"."lesson_keywords" from "anon";
+
+revoke truncate on table "public"."lesson_keywords" from "anon";
+
+revoke update on table "public"."lesson_keywords" from "anon";
+
+revoke delete on table "public"."lesson_keywords" from "authenticated";
+
+revoke insert on table "public"."lesson_keywords" from "authenticated";
+
+revoke references on table "public"."lesson_keywords" from "authenticated";
+
+revoke select on table "public"."lesson_keywords" from "authenticated";
+
+revoke trigger on table "public"."lesson_keywords" from "authenticated";
+
+revoke truncate on table "public"."lesson_keywords" from "authenticated";
+
+revoke update on table "public"."lesson_keywords" from "authenticated";
+
+revoke delete on table "public"."lesson_keywords" from "service_role";
+
+revoke insert on table "public"."lesson_keywords" from "service_role";
+
+revoke references on table "public"."lesson_keywords" from "service_role";
+
+revoke select on table "public"."lesson_keywords" from "service_role";
+
+revoke trigger on table "public"."lesson_keywords" from "service_role";
+
+revoke truncate on table "public"."lesson_keywords" from "service_role";
+
+revoke update on table "public"."lesson_keywords" from "service_role";
+
+revoke delete on table "public"."lessons" from "anon";
+
+revoke insert on table "public"."lessons" from "anon";
+
+revoke references on table "public"."lessons" from "anon";
+
+revoke select on table "public"."lessons" from "anon";
+
+revoke trigger on table "public"."lessons" from "anon";
+
+revoke truncate on table "public"."lessons" from "anon";
+
+revoke update on table "public"."lessons" from "anon";
+
+revoke delete on table "public"."lessons" from "authenticated";
+
+revoke insert on table "public"."lessons" from "authenticated";
+
+revoke references on table "public"."lessons" from "authenticated";
+
+revoke select on table "public"."lessons" from "authenticated";
+
+revoke trigger on table "public"."lessons" from "authenticated";
+
+revoke truncate on table "public"."lessons" from "authenticated";
+
+revoke update on table "public"."lessons" from "authenticated";
+
+revoke delete on table "public"."lessons" from "service_role";
+
+revoke insert on table "public"."lessons" from "service_role";
+
+revoke references on table "public"."lessons" from "service_role";
+
+revoke select on table "public"."lessons" from "service_role";
+
+revoke trigger on table "public"."lessons" from "service_role";
+
+revoke truncate on table "public"."lessons" from "service_role";
+
+revoke update on table "public"."lessons" from "service_role";
+
+revoke delete on table "public"."profiles" from "anon";
+
+revoke insert on table "public"."profiles" from "anon";
+
+revoke references on table "public"."profiles" from "anon";
+
+revoke select on table "public"."profiles" from "anon";
+
+revoke trigger on table "public"."profiles" from "anon";
+
+revoke truncate on table "public"."profiles" from "anon";
+
+revoke update on table "public"."profiles" from "anon";
+
+revoke delete on table "public"."profiles" from "authenticated";
+
+revoke insert on table "public"."profiles" from "authenticated";
+
+revoke references on table "public"."profiles" from "authenticated";
+
+revoke select on table "public"."profiles" from "authenticated";
+
+revoke trigger on table "public"."profiles" from "authenticated";
+
+revoke truncate on table "public"."profiles" from "authenticated";
+
+revoke update on table "public"."profiles" from "authenticated";
+
+revoke delete on table "public"."profiles" from "service_role";
+
+revoke insert on table "public"."profiles" from "service_role";
+
+revoke references on table "public"."profiles" from "service_role";
+
+revoke select on table "public"."profiles" from "service_role";
+
+revoke trigger on table "public"."profiles" from "service_role";
+
+revoke truncate on table "public"."profiles" from "service_role";
+
+revoke update on table "public"."profiles" from "service_role";
+
+revoke delete on table "public"."subscriptions" from "anon";
+
+revoke insert on table "public"."subscriptions" from "anon";
+
+revoke references on table "public"."subscriptions" from "anon";
+
+revoke select on table "public"."subscriptions" from "anon";
+
+revoke trigger on table "public"."subscriptions" from "anon";
+
+revoke truncate on table "public"."subscriptions" from "anon";
+
+revoke update on table "public"."subscriptions" from "anon";
+
+revoke delete on table "public"."subscriptions" from "authenticated";
+
+revoke insert on table "public"."subscriptions" from "authenticated";
+
+revoke references on table "public"."subscriptions" from "authenticated";
+
+revoke select on table "public"."subscriptions" from "authenticated";
+
+revoke trigger on table "public"."subscriptions" from "authenticated";
+
+revoke truncate on table "public"."subscriptions" from "authenticated";
+
+revoke update on table "public"."subscriptions" from "authenticated";
+
+revoke delete on table "public"."subscriptions" from "service_role";
+
+revoke insert on table "public"."subscriptions" from "service_role";
+
+revoke references on table "public"."subscriptions" from "service_role";
+
+revoke select on table "public"."subscriptions" from "service_role";
+
+revoke trigger on table "public"."subscriptions" from "service_role";
+
+revoke truncate on table "public"."subscriptions" from "service_role";
+
+revoke update on table "public"."subscriptions" from "service_role";
+
+revoke delete on table "public"."user_favorites" from "anon";
+
+revoke insert on table "public"."user_favorites" from "anon";
+
+revoke references on table "public"."user_favorites" from "anon";
+
+revoke select on table "public"."user_favorites" from "anon";
+
+revoke trigger on table "public"."user_favorites" from "anon";
+
+revoke truncate on table "public"."user_favorites" from "anon";
+
+revoke update on table "public"."user_favorites" from "anon";
+
+revoke delete on table "public"."user_favorites" from "authenticated";
+
+revoke insert on table "public"."user_favorites" from "authenticated";
+
+revoke references on table "public"."user_favorites" from "authenticated";
+
+revoke select on table "public"."user_favorites" from "authenticated";
+
+revoke trigger on table "public"."user_favorites" from "authenticated";
+
+revoke truncate on table "public"."user_favorites" from "authenticated";
+
+revoke update on table "public"."user_favorites" from "authenticated";
+
+revoke delete on table "public"."user_favorites" from "service_role";
+
+revoke insert on table "public"."user_favorites" from "service_role";
+
+revoke references on table "public"."user_favorites" from "service_role";
+
+revoke select on table "public"."user_favorites" from "service_role";
+
+revoke trigger on table "public"."user_favorites" from "service_role";
+
+revoke truncate on table "public"."user_favorites" from "service_role";
+
+revoke update on table "public"."user_favorites" from "service_role";
+
+revoke delete on table "public"."user_learned_lessons" from "anon";
+
+revoke insert on table "public"."user_learned_lessons" from "anon";
+
+revoke references on table "public"."user_learned_lessons" from "anon";
+
+revoke select on table "public"."user_learned_lessons" from "anon";
+
+revoke trigger on table "public"."user_learned_lessons" from "anon";
+
+revoke truncate on table "public"."user_learned_lessons" from "anon";
+
+revoke update on table "public"."user_learned_lessons" from "anon";
+
+revoke delete on table "public"."user_learned_lessons" from "authenticated";
+
+revoke insert on table "public"."user_learned_lessons" from "authenticated";
+
+revoke references on table "public"."user_learned_lessons" from "authenticated";
+
+revoke select on table "public"."user_learned_lessons" from "authenticated";
+
+revoke trigger on table "public"."user_learned_lessons" from "authenticated";
+
+revoke truncate on table "public"."user_learned_lessons" from "authenticated";
+
+revoke update on table "public"."user_learned_lessons" from "authenticated";
+
+revoke delete on table "public"."user_learned_lessons" from "service_role";
+
+revoke insert on table "public"."user_learned_lessons" from "service_role";
+
+revoke references on table "public"."user_learned_lessons" from "service_role";
+
+revoke select on table "public"."user_learned_lessons" from "service_role";
+
+revoke trigger on table "public"."user_learned_lessons" from "service_role";
+
+revoke truncate on table "public"."user_learned_lessons" from "service_role";
+
+revoke update on table "public"."user_learned_lessons" from "service_role";
+
+
+
